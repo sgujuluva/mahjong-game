52a474c5dc55578d141c687327899109d1ac2397
sgujuluva <sanghee04@gmail.com>
2022-06-10 15:51:06 +0200

    trying to remove 1st box and second box

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const container = document.querySelector(".container");
 const span = document.querySelector("h2 span");
 let markedId = -1;
+let markedText = 0;
 //to create 10 div
-function divs(){
-    for(let i = 0; i< 10; i++){
-        let box = document.createElement("div");
-        // generate random numbers
-        let randomNumber = Math.ceil(Math.random() * 10);
-        let textBox = document.createTextNode(randomNumber);
-        box.appendChild(textBox);
-        container.appendChild(box); 
-        //to create id
-        box.id = i;
-                 // click div
-        box.addEventListener("click", (e) => {
-           // select the 1st box with id
-           let firstClickBox = document.getElementById(e.target.id);
-           //mark it
-           if(markedId === -1){
-                markedId = e.target.id;
-                firstClickBox.classList.add("marked");
-           }
-            
-        
-})
-     
-    }
+function divs() {
+  for (let i = 0; i < 10; i++) {
+    let box = document.createElement("div");
+    // generate random numbers
+    let randomNumber = Math.ceil(Math.random() * 10);
+    let textBox = document.createTextNode(randomNumber);
+    box.appendChild(textBox);
+    container.appendChild(box);
+    //to create id
+    box.id = i;
+    // click div
+    box.addEventListener("click", (e) => {
+      // select the 1st box with id
+      let firstClickBox = document.getElementById(e.target.id);
+      //mark it
+      if (markedId === -1) {
+        markedId = e.target.id;
+        markedText = e.target.innerText;
+        firstClickBox.classList.add("marked");
+      } else {
+        if(markedText === e.target.innerText){
+            box.remove();
+            firstClickBox.remove();
+        }
+      }
+    });
+  }
 }
 divs();
-
-  
