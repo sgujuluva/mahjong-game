2d1a6151e552cc0b6061b4364aba8faf313a675d
sgujuluva <sanghee04@gmail.com>
2022-06-13 10:30:47 +0200

    selecting the boxes

--- index.js
+++ index.js
@@ -2,11 +2,41 @@ const container = document.querySelector(".container");
 const span = document.querySelector("h2 span");
 
 // to create 10 divs and append random numbers to the respective boxes
+for(let i = 0; i < 10; i++){
+  let boxes = document.createElement("div");
+  let randomNumbers = Math.ceil(Math.random() * 10);
+  let text = document.createTextNode(randomNumbers);
+  boxes.appendChild(text);
+  container.appendChild(boxes);
 
-let boxes = document.createElement("div");
-let randomNumbers = Math.ceil(Math.random() * 10);
-let text = document.createTextNode(randomNumbers);
-boxes.appendChild(text);
-container.appendChild(boxes);
+  // create id to each box
+boxes.id = i;
+//console.log(boxes);
+
+// click a box
+boxes.addEventListener("click", (e) => {
+  //mark the clicked box with id
+  let clickBox = document.getElementById(e.target.id);
+  clickBox.classList.toggle("marked");
+
+  // get the clicked boxes but only 2
+  let selectBoxes = document.querySelectorAll(".marked");
+  selectBoxes.forEach(selectBox => {
+    if(selectBox.length > 2){
+      boxes.classList.remove("marked");
+    }
+  });
+
+
+
+  
+});
+
+
+
+
+
+}
+
 
 
