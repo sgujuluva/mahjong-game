765a8b4b5820647da32fa0253d6a428ad2dd5c31
sgujuluva <sanghee04@gmail.com>
2022-06-13 10:19:01 +0200

    10 div s created

--- index.js
+++ index.js
@@ -1,66 +1,12 @@
 const container = document.querySelector(".container");
 const span = document.querySelector("h2 span");
-let markedId = -1;
-let markedText = 0;
-let mistakes = 0;
-//to create 10 div
-function divs() {
-  for (let i = 0; i < 10; i++) {
-    let box = document.createElement("div");
-    // generate random numbers
-    let randomNumber = Math.ceil(Math.random() * 10);
-    let textBox = document.createTextNode(randomNumber);
-    box.appendChild(textBox);
-    container.appendChild(box);
-    //to create id
-    box.id = i;
-    // click div
-    box.addEventListener("click", (e) => {
-      // select the 1st box with id
-      let firstClickBox = document.getElementById(e.target.id);
-      let firstClickBoxText = firstClickBox.innerText;
-console.log("firstClickBoxText",firstClickBoxText)
-      // to select the first clicked box with the Id
-      if (markedId === -1) {
-        firstClickBox.classList.add("marked");
-        markedId = e.target.id;
-      } else {
-         // if same box clicked again
-        if (markedId === e.target.id) {
-          firstClickBox.classList.remove("marked");
-          markedId = -1;
-        }  else {
-          // another box clicked
-          if (!firstClickBoxText == e.target.innerText) {
-            console.log("firstClickBoxText1",firstClickBoxText)
-            console.log(e.target.innerText)
-            // user clicked correct box
-       
-          
-          }else{
-            removeBox(e.target.id, markedId);
-          }
-        }
-        markedId = -1;     
-      }
 
-    });  
+// to create 10 divs and append random numbers to the respective boxes
 
-  }
-}
-divs();
-// remove the boxes which has same text
-function removeBox(a, b) {
-  let box1 = document.getElementById(a);
-  let box2 = document.getElementById(b);
-  console.log(box1)
-  console.log(box2)
-  box1.remove();
-  box2.remove();
-}
-// update mistakes
-function scoreMistakes() {
-  let score = document.querySelector("h2");
-  console.log(score)
-  score.textContent =  `${mistakes}`;
-}
+let boxes = document.createElement("div");
+let randomNumbers = Math.ceil(Math.random() * 10);
+let text = document.createTextNode(randomNumbers);
+boxes.appendChild(text);
+container.appendChild(boxes);
+
+
