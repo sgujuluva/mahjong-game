b946bf9efb8ba9a7d89b90f356f8d36f13fa75bb
sgujuluva <sanghee04@gmail.com>
2022-06-14 19:47:34 +0200

    working on mistakes increase

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 const span = document.querySelector("h2 span");
 
+let mistakes = 0;
 // to create 10 divs and append random numbers to the respective boxes
 for(let i = 0; i < 10; i++){
   let boxes = document.createElement("div");
@@ -20,23 +21,32 @@ boxes.addEventListener("click", (e) => {
   clickBox.classList.toggle("marked");
 
   // get the clicked boxes but only 2
-  let selectBoxes = document.querySelectorAll(".marked");
-  selectBoxes.forEach(selectBox => {
-    if(selectBox.length > 2){
-      boxes.classList.remove("marked");
-    }
-  });
+       const allBoxes = document.querySelectorAll(".marked");
+      allBoxes.forEach((box) => {
+        if (allBoxes.length > 2) {
+          box.classList.remove("marked");
+        }
+      });
+      for (let i = 0; i < allBoxes.length; i++) {
+        const box1 = allBoxes[0];
+        const box2 = allBoxes[1];
+        if (allBoxes.length > 1 && box1.innerText === box2.innerText) {
+          box1.remove();
+          box2.remove();
+        }/* else if(box1.innerText !== box2.innerText){
+          mistakes ++;
+        }
+          
+         */  /*  need to Worker */
+      }
 
-
-
-  
 });
-
-
-
-
-
+console.log("The mistakes are : ", mistakes)
 }
 
-
-
+function scoreMistakes() {
+  let score = document.querySelector("h2");
+  console.log(score);
+  score.textContent = `${mistakes}`;
+  return score;
+}
